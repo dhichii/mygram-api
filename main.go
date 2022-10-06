4844bfed6f9a6a0b3c110e400c62853261e4cc93
Adhicitta Masran <adhicittamasran@gmail.com>
2022-10-06 14:03:29 +0800

    chore: init server

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "mygram-api/src/database"
+import (
+	"mygram-api/src/config/env"
+	"mygram-api/src/database"
+	"mygram-api/src/router"
+)
 
 func main() {
 	database.InitPostgres()
+	router.StartServer().Run(":" + env.GetServerEnv())
 }
