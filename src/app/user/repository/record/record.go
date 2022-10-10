b75b3c882606593b4185af8984371117cb9552e7
Adhicitta Masran <adhicittamasran@gmail.com>
2022-10-10 14:21:20 +0800

    feat(user): update gorm tag in the record
    secure username, email, password, and age be not null. make username and email be unique

--- src/app/user/repository/record/record.go
+++ src/app/user/repository/record/record.go
@@ -4,10 +4,10 @@ import "time"
 
 type User struct {
 	ID        int    `gorm:"primaryKey"`
-	Username  string `gorm:"type:varchar"`
-	Email     string `gorm:"type:varchar"`
-	Password  string
-	Age       int
+	Username  string `gorm:"type:varchar;unique;not null"`
+	Email     string `gorm:"type:varchar;unique;not null"`
+	Password  string `gorm:"not null"`
+	Age       int    `gorm:"not null"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
