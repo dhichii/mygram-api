fdb5c1feb273d792eb7bab1978b35e8cf16fbaa0
Adhicitta Masran <adhicittamasran@gmail.com>
2022-10-10 17:36:24 +0800

    feat(photo): add interfaces

---
+++ src/app/photo/interfaces.go
@@ -0,0 +1,18 @@
+package photo
+
+import "mygram-api/src/app/photo/repository/record"
+
+type Service interface {
+	CreatePhoto(photo *record.Photo) (*record.Photo, error)
+	GetAllPhotos() ([]record.Photo, error)
+	UpdatePhoto(id int, photo *record.Photo) (*record.Photo, error)
+	DeletePhoto(id int) error
+}
+
+type Repository interface {
+	CreateData(data *record.Photo) (*record.Photo, error)
+	GetAllData() ([]record.Photo, error)
+	UpdateData(id int, data *record.Photo) (*record.Photo, error)
+	GetUserIDByID(id int) *string
+	DeleteData(id int) error
+}
