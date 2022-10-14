2242025a3d26eae5be0ea2be0933611f4cb404d6
Adhicitta Masran <adhicittamasran@gmail.com>
2022-10-14 21:47:04 +0800

    refactor(social-media): add CreatedAt & UpdatedAt in the record

--- src/app/socialmedia/repository/record/record.go
+++ src/app/socialmedia/repository/record/record.go
@@ -1,10 +1,15 @@
 package record
 
+import "time"
+
 type SocialMedia struct {
 	ID             int    `gorm:"primaryKey"`
 	Name           string `gorm:"type:varchar;not null"`
 	SocialMediaUrl string `gorm:"not null"`
 	UserID         int
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+	User           User
 }
 
 type User struct {
