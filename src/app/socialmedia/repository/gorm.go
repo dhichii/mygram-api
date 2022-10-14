e9dd5909d705a1adc288ed587724ab4d8a1b3dd0
Adhicitta Masran <adhicittamasran@gmail.com>
2022-10-14 20:52:05 +0800

    feat(social-media): implement repo

---
+++ src/app/socialmedia/repository/gorm.go
@@ -0,0 +1,68 @@
+package repository
+
+import (
+	"errors"
+	"mygram-api/src/app/socialmedia"
+	"mygram-api/src/app/socialmedia/repository/record"
+	"mygram-api/src/helper"
+
+	"gorm.io/gorm"
+)
+
+type repository struct {
+	db *gorm.DB
+}
+
+func (repo *repository) CreateData(data *record.SocialMedia) (*record.SocialMedia, error) {
+	if err := repo.db.Create(data).Error; err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func (repo *repository) GetAllData() ([]record.SocialMedia, error) {
+	records := []record.SocialMedia{}
+	if err := repo.db.Find(&records).Error; err != nil {
+		return nil, err
+	}
+
+	return records, nil
+}
+
+func (repo *repository) GetUserIDByID(id int) (int, error) {
+	userId := 0
+	if err := repo.db.Model(new(record.SocialMedia)).Select("user_id").First(&userId, "id", id).Error; err != nil {
+		return 0, err
+	}
+
+	return userId, nil
+}
+
+func (repo *repository) UpdateData(id int, data *record.SocialMedia) (*record.SocialMedia, error) {
+	query := repo.db.Where("id", id).Updates(data)
+	err := query.Error
+	if err != nil {
+		return nil, err
+	}
+
+	if err == nil && query.RowsAffected < 1 {
+		return nil, errors.New(helper.NOTFOUND)
+	}
+
+	data.ID = id
+	return data, nil
+}
+
+func (repo *repository) DeleteData(id int) error {
+	query := repo.db.Delete(new(record.SocialMedia), "id", id)
+	if query.Error == nil && query.RowsAffected < 1 {
+		return errors.New(helper.NOTFOUND)
+	}
+
+	return query.Error
+}
+
+func NewGORMRepository(db *gorm.DB) socialmedia.Repository {
+	return &repository{db}
+}
