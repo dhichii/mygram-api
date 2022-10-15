d26b546747f1278aa5677ee3abb799a509a5a1d1
Adhicitta Masran <adhicittamasran@gmail.com>
2022-10-15 14:59:04 +0800

    feat(social-media): add factory and adapter

--- src/adapter/adapters.go
+++ src/adapter/adapters.go
@@ -3,20 +3,24 @@ package adapter
 import (
 	photo_factory "mygram-api/src/app/photo/factory"
 	photo_handler "mygram-api/src/app/photo/handler"
+	social_media_factory "mygram-api/src/app/socialmedia/factory"
+	social_media_handler "mygram-api/src/app/socialmedia/handler"
 	user_factory "mygram-api/src/app/user/factory"
 	user_handler "mygram-api/src/app/user/handler"
 	"mygram-api/src/database"
 )
 
 type handlers struct {
-	User  user_handler.Handler
-	Photo photo_handler.Handler
+	User        user_handler.Handler
+	Photo       photo_handler.Handler
+	SocialMedia social_media_handler.Handler
 }
 
 func Init() handlers {
 	conn := database.GetPostgresConnection()
 	return handlers{
-		User:  user_factory.Factory(conn),
-		Photo: photo_factory.Factory(conn),
+		User:        user_factory.Factory(conn),
+		Photo:       photo_factory.Factory(conn),
+		SocialMedia: social_media_factory.Factory(conn),
 	}
 }
