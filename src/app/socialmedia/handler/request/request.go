c9d86deae3ca04e921e57fbde171d7406419c866
Adhicitta Masran <adhicittamasran@gmail.com>
2022-10-15 12:45:36 +0800

    feat(social-media): add request and response function dto

--- src/app/socialmedia/handler/request/request.go
+++ src/app/socialmedia/handler/request/request.go
@@ -1,6 +1,16 @@
 package request
 
+import "mygram-api/src/app/socialmedia/repository/record"
+
 type Request struct {
 	Name           string `json:"name"`
 	SocialMediaUrl string `json:"social_media_url"`
+}
+
+func (r *Request) MapToRecord(userID int) *record.SocialMedia {
+	return &record.SocialMedia{
+		Name:           r.Name,
+		SocialMediaUrl: r.SocialMediaUrl,
+		UserID:         userID,
+	}
 }
--- src/app/socialmedia/handler/response/response.go
+++ src/app/socialmedia/handler/response/response.go
@@ -1,6 +1,9 @@
 package response
 
-import "time"
+import (
+	"mygram-api/src/app/socialmedia/repository/record"
+	"time"
+)
 
 type PostResponse struct {
 	ID             int       `json:"id"`
@@ -35,4 +38,48 @@ type UpdateResponse struct {
 	SocialMediaUrl string    `json:"social_media_url"`
 	UserID         int       `json:"user_id"`
 	UpdatedAt      time.Time `json:"updated_at"`
+}
+
+func MapToPostResponse(res record.SocialMedia) PostResponse {
+	return PostResponse{
+		ID:             res.ID,
+		Name:           res.Name,
+		SocialMediaUrl: res.SocialMediaUrl,
+		UserID:         res.UserID,
+		CreatedAt:      res.CreatedAt,
+	}
+}
+
+func MapToBatchSocialMediaResponse(res []record.SocialMedia) Response {
+	socialMedias := []SocialMediaResponse{}
+	for _, socialMedia := range res {
+		socialMedias = append(socialMedias, MapToSocialMediaResponse(socialMedia))
+	}
+
+	return Response{SocialMedias: socialMedias}
+}
+
+func MapToSocialMediaResponse(res record.SocialMedia) SocialMediaResponse {
+	return SocialMediaResponse{
+		ID:             res.ID,
+		Name:           res.Name,
+		SocialMediaUrl: res.SocialMediaUrl,
+		UserID:         res.UserID,
+		CreatedAt:      res.CreatedAt,
+		UpdatedAt:      res.UpdatedAt,
+		User: UserResponse{
+			ID:       res.UserID,
+			Username: res.User.Username,
+		},
+	}
+}
+
+func MapToUpdateResponse(res record.SocialMedia) UpdateResponse {
+	return UpdateResponse{
+		ID:             res.ID,
+		Name:           res.Name,
+		SocialMediaUrl: res.SocialMediaUrl,
+		UserID:         res.UserID,
+		UpdatedAt:      res.UpdatedAt,
+	}
 }
