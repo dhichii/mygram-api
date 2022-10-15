3cad1138285207dd8a636d6f01b2537127eaff85
Adhicitta Masran <adhicittamasran@gmail.com>
2022-10-15 21:33:09 +0800

    feat(comment): add request and response function dto

--- src/app/comment/handler/request/request.go
+++ src/app/comment/handler/request/request.go
@@ -1,5 +1,7 @@
 package request
 
+import "mygram-api/src/app/comment/repository/record"
+
 type PostRequest struct {
 	Message string `json:"message"`
 	PhotoID int    `json:"photo_id"`
@@ -7,4 +9,12 @@ type PostRequest struct {
 
 type UpdateRequest struct {
 	Message string `json:"message"`
+}
+
+func (req *PostRequest) MapToRecord(userId int) *record.Comment {
+	return &record.Comment{
+		UserID:  userId,
+		Message: req.Message,
+		PhotoID: req.PhotoID,
+	}
 }
