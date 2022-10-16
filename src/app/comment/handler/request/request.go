1d8ae401a0061e5f00d64b042ca9366862fd1f61
Adhicitta Masran <adhicittamasran@gmail.com>
2022-10-16 17:43:36 +0800

    docs(comment): add request and response example

--- src/app/comment/handler/request/request.go
+++ src/app/comment/handler/request/request.go
@@ -3,12 +3,12 @@ package request
 import "mygram-api/src/app/comment/repository/record"
 
 type PostRequest struct {
-	Message string `json:"message" binding:"required"`
-	PhotoID int    `json:"photo_id" binding:"required"`
+	Message string `json:"message" binding:"required" example:"example"`
+	PhotoID int    `json:"photo_id" binding:"required" example:"1"`
 }
 
 type UpdateRequest struct {
-	Message string `json:"message" binding:"required"`
+	Message string `json:"message" binding:"required" example:"example"`
 }
 
 func (req *PostRequest) MapToRecord(userId int) *record.Comment {
