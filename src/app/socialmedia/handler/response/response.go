3f0ca4dde9bf4f011e4bb25c1cac55f653d921f2
Adhicitta Masran <adhicittamasran@gmail.com>
2022-10-16 17:31:03 +0800

    docs(social-media): add request and response example

--- src/app/socialmedia/handler/response/response.go
+++ src/app/socialmedia/handler/response/response.go
@@ -6,11 +6,11 @@ import (
 )
 
 type PostResponse struct {
-	ID             int       `json:"id"`
-	Name           string    `json:"name"`
-	SocialMediaUrl string    `json:"social_media_url"`
-	UserID         int       `json:"user_id"`
-	CreatedAt      time.Time `json:"created_at"`
+	ID             int       `json:"id" example:"1"`
+	Name           string    `json:"name" example:"example"`
+	SocialMediaUrl string    `json:"social_media_url" example:"/example"`
+	UserID         int       `json:"user_id" example:"1"`
+	CreatedAt      time.Time `json:"created_at" example:"2022-10-07T15:54:24.575005+07:00"`
 }
 
 type Response struct {
@@ -18,26 +18,26 @@ type Response struct {
 }
 
 type SocialMediaResponse struct {
-	ID             int          `json:"id"`
-	Name           string       `json:"name"`
-	SocialMediaUrl string       `json:"social_media_url"`
-	UserID         int          `json:"user_id"`
-	CreatedAt      time.Time    `json:"created_at"`
-	UpdatedAt      time.Time    `json:"updated_at"`
+	ID             int          `json:"id" example:"1"`
+	Name           string       `json:"name" example:"example"`
+	SocialMediaUrl string       `json:"social_media_url" example:"/example"`
+	UserID         int          `json:"user_id" example:"1"`
+	CreatedAt      time.Time    `json:"created_at" example:"2022-10-07T15:54:24.575005+07:00"`
+	UpdatedAt      time.Time    `json:"updated_at" example:"2022-10-07T15:54:24.575005+07:00"`
 	User           UserResponse `json:"User"`
 }
 
 type UserResponse struct {
-	ID       int    `json:"id"`
-	Username string `json:"username"`
+	ID       int    `json:"id" example:"1"`
+	Username string `json:"username" example:"example"`
 }
 
 type UpdateResponse struct {
-	ID             int       `json:"id"`
-	Name           string    `json:"name"`
-	SocialMediaUrl string    `json:"social_media_url"`
-	UserID         int       `json:"user_id"`
-	UpdatedAt      time.Time `json:"updated_at"`
+	ID             int       `json:"id" example:"1"`
+	Name           string    `json:"name" example:"example"`
+	SocialMediaUrl string    `json:"social_media_url" example:"/example"`
+	UserID         int       `json:"user_id" example:"1"`
+	UpdatedAt      time.Time `json:"updated_at" example:"2022-10-07T15:54:24.575005+07:00"`
 }
 
 func MapToPostResponse(res record.SocialMedia) PostResponse {
