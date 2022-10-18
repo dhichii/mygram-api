9485210b1e1a9cc65d267ed1cb9d78bf3ebd04d5
Adhicitta Masran <adhicittamasran@gmail.com>
2022-10-18 17:17:16 +0800

    refactor(auth): implement error message helper and handle http param

--- src/app/auth/service/services.go
+++ src/app/auth/service/services.go
@@ -1,20 +1,23 @@
 package service
 
-import "mygram-api/src/app/auth"
+import (
+	"mygram-api/src/app/auth"
+	"mygram-api/src/helper/errs"
+)
 
 type service struct {
 	repo auth.Repository
 }
 
-func (s *service) GetUserIDByPhotoID(photoId int) (int, error) {
+func (s *service) GetUserIDByPhotoID(photoId int) (int, errs.MessageErr) {
 	return s.repo.GetUserIDByFeatureID("photos", photoId)
 }
 
-func (s *service) GetUserIDByCommentID(commentId int) (int, error) {
+func (s *service) GetUserIDByCommentID(commentId int) (int, errs.MessageErr) {
 	return s.repo.GetUserIDByFeatureID("comments", commentId)
 }
 
-func (s *service) GetUserIDBySocialMediaID(socialMediaId int) (int, error) {
+func (s *service) GetUserIDBySocialMediaID(socialMediaId int) (int, errs.MessageErr) {
 	return s.repo.GetUserIDByFeatureID("social_media", socialMediaId)
 }
 
